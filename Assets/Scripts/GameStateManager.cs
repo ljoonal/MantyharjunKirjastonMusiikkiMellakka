e0870ef7ce8a9bb6ec14d4d032f511e81251a846
ljoonal <ljoonal@ljoonal.xyz>
2021-12-14 12:31:48 +0200

    Add a forgotten ;

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -32,7 +32,7 @@ public class GameStateManager : MonoBehaviour
 	void FixedUpdate()
 	{
 		time += Time.deltaTime;
-		timeText.text = $"Aika: {Math.Round(time)}/{Math.Round(timeLimit)}"
+		timeText.text = $"Aika: {Math.Round(time)}/{Math.Round(timeLimit)}";
 
 
 		if (time > timeLimit)
